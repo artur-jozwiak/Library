e6a093157ff9119e05b6504dbb58217d5663cd68
Artur Józwiak <artur_jozwiak@o2.pl>
2022-04-15 18:29:33 +0200

    Database connected

---
+++ Library/Library.Database/Migrations/20220415161712_Initial.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Database.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonalNumber = table.Column<int>(type: "int", nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RentInterval = table.Column<int>(type: "int", nullable: false),
+                    Cost = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_UserId",
+                table: "Books",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_BookId",
+                table: "Orders",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Library/Library.Database/Program.cs
+++ Library/Library.Database/Program.cs
@@ -1,1 +1,64 @@
 ﻿
+using Library.BussinesLogic.Models;
+using Library.Database;
+using Microsoft.EntityFrameworkCore;
+
+using (var context = new LibraryContext())
+{
+    var book = new Book
+    {
+        Title = "Hary Potter",
+        Author = " J.K. Rowling",
+        Category = Library.BussinesLogic.Enums.BookCategory.SciFi,
+        Quantity = 3
+
+    };
+    new Book
+    {
+        Title = "Clean Code",
+        Author = "Robert Cecil Martin",
+        Category = Library.BussinesLogic.Enums.BookCategory.Fantasy,
+        Quantity = 2
+
+    };
+    new Book
+    {
+        Title = "Metro",
+        Author = "Dimitry Gluhovsky",
+        Category = Library.BussinesLogic.Enums.BookCategory.SciFi,
+        Quantity = 2
+
+    };
+
+    await context.AddAsync(book);
+    await context.SaveChangesAsync();
+
+
+
+
+    //var books = await context.Books.ToArrayAsync();
+    var user = new User
+    {
+        Name = "Marcin",
+        Surname = "Kowalski",
+        PersonalNumber = 5121376,
+        Role = Library.BussinesLogic.Enums.Role.Employee
+    };
+    await context.AddAsync(user);
+    await context.SaveChangesAsync();
+
+    var order = new Order
+    {
+        Book = book,
+        User = user,
+        UserId = 1,
+        BookId = 3,
+        StartTime = DateTime.Now,
+        EndTime = DateTime.Now,
+        RentInterval = 3,
+        Cost = 0
+
+    };
+    await context.AddAsync(order);
+    await context.SaveChangesAsync();
+}
